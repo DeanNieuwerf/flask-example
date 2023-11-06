880583441eab41332a610d74014061a54ea700f3
Deantjuh <dean.nieuwerf@quicknet.nl>
2023-11-06 16:52:46 +0100

    Added HTML templat

--- application.py
+++ application.py
@@ -1,7 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template, request, jsonify
 
 application = Flask(__name__)
 
+# Store chat messages in a list
+chat_messages = []
+
 @application.route('/')
-def hello_world():
-    return 'Hoi Dit is Dean van Praxis'
+def home():
+    return render_template('index.html', chat_messages=chat_messages)
+
+@application.route('/send', methods=['POST'])
+def send_message():
+    message = request.form.get('message')
+    if message:
+        chat_messages.append(message)
+    return jsonify({'status': 'OK'})
+
+if __name__ == '__main__':
+    application.run(debug=True)
